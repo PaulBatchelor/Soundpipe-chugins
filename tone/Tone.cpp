8e9cedb002f191c0398a048f8c7176218f7eb067
paulbatchelor <thisispaulbatchelor@gmail.com>
2015-07-02 20:15:43 -0400

    things compile

--- tone/Tone.cpp
+++ tone/Tone.cpp
@@ -5,28 +5,31 @@
 
 #include <stdio.h>
 #include <limits.h>
-
+#include <soundpipe.h>
 
 CK_DLL_CTOR(tone_ctor);
 CK_DLL_DTOR(tone_dtor);
 
-CK_DLL_MFUN(tone_setBits);
-CK_DLL_MFUN(tone_getBits);
+CK_DLL_MFUN(tone_setCutoff);
+CK_DLL_MFUN(tone_getCutoff);
 
 CK_DLL_TICK(tone_tick);
 
 t_CKINT tone_data_offset = 0;
+/*
+struct ToneData {
+    float foo;
+    sp_data *sp;
+    sp_tone *tone;
+};
+*/
 
-
-struct ToneData
-{
-    int bits;
-    int downsampleFactor;
-    
-    int currentSampleCount;
-    SAMPLE currentSample;
+struct ToneData {
+    float foo;
+    sp_data *sp;
 };
 
+static ToneData global_data;
 
 CK_DLL_QUERY(Bitcrusher)
 {
@@ -39,10 +42,10 @@ CK_DLL_QUERY(Bitcrusher)
     
     QUERY->add_ugen_func(QUERY, tone_tick, NULL, 1, 1);
     
-    QUERY->add_mfun(QUERY, tone_setBits, "int", "bits");
-    QUERY->add_arg(QUERY, "int", "arg");
+    QUERY->add_mfun(QUERY, tone_setCutoff, "float", "cutoff");
+    QUERY->add_arg(QUERY, "float", "arg");
     
-    QUERY->add_mfun(QUERY, tone_getBits, "int", "bits");
+    QUERY->add_mfun(QUERY, tone_getCutoff, "float", "cutoff");
     
     tone_data_offset = QUERY->add_mvar(QUERY, "int", "@tone_data", false);
     
@@ -56,7 +59,13 @@ CK_DLL_CTOR(tone_ctor)
 {
     OBJ_MEMBER_INT(SELF, tone_data_offset) = 0;
     
+    
     ToneData * data = new ToneData;
+    
+    //p_create(&data->sp);
+    //data->sp->sr = API->vm->get_srate();
+    //sp_tone_create(&data->tone);
+    //sp_tone_init(data->sp, data->tone);
     
     OBJ_MEMBER_INT(SELF, tone_data_offset) = (t_CKINT) data;
 }
@@ -66,6 +75,8 @@ CK_DLL_DTOR(tone_dtor)
     ToneData * data = (ToneData *) OBJ_MEMBER_INT(SELF, tone_data_offset);
     if(data)
     {
+        //sp_tone_destroy(&data->tone);
+        //sp_destroy(&data->sp);
         delete data;
         OBJ_MEMBER_INT(SELF, tone_data_offset) = 0;
         data = NULL;
@@ -76,23 +87,22 @@ CK_DLL_TICK(tone_tick)
 {
     ToneData * data = (ToneData *) OBJ_MEMBER_INT(SELF, tone_data_offset);
     
-    
     *out = in;
 
     return TRUE;
 }
 
-CK_DLL_MFUN(tone_setBits)
+CK_DLL_MFUN(tone_setCutoff)
 {
     ToneData * data = (ToneData *) OBJ_MEMBER_INT(SELF, tone_data_offset);
     // TODO: sanity check
-    data->bits = GET_NEXT_INT(ARGS);
-    RETURN->v_int = data->bits;
+    data->foo = GET_NEXT_FLOAT(ARGS);
+    RETURN->v_float = data->foo;
 }
 
-CK_DLL_MFUN(tone_getBits)
+CK_DLL_MFUN(tone_getCutoff)
 {
     ToneData * data = (ToneData *) OBJ_MEMBER_INT(SELF, tone_data_offset);
-    RETURN->v_int = data->bits;
+    RETURN->v_float = data->foo;
 }
 
